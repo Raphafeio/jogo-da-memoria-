3d9eb8b9e44c5d561934812c1a9decded4aed690
Raphafeio <111800364+Raphafeio@users.noreply.github.com>
2023-06-03 16:21:33 -0400

    Add files via upload

---
+++ Jogo da Memoria/pages/login.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login-input');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
+
+const validadeInput = ({ target}) => {
+   if (target.value.length > 3) {
+    button.removeAttribute('disabled');
+    return;
+   } 
+    
+   button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = 'game.html';
+}
+
+input.addEventListener('input', validadeInput);
+form.addEventListener('submit', handleSubmit);
